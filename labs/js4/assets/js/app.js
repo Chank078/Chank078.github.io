42e29575372bacc5db20f326d17c457213b83f54
Kimberly Chan <chank078@newschool.edu>
2017-11-07 10:54:50 -0500

    lab4

--- labs/js4/assets/js/app.js
+++ labs/js4/assets/js/app.js
@@ -1,54 +1,35 @@
-// $('.nav-item').click(function(){
-//  	if($(this).hasClass('apple-button')){
-//  		$('.apple').removeClass('hide');
-//  	}
-// 	else if($(this).hasClass('nike-button')){
-//  		$('.nike').removeClass('hide');
-//  	}
-//  	else if($(this).hasClass('disney-button')){
-//  		$('.disney').removeClass('hide');
-//  	} 
-//  	else{
-//  		$('.vogue').removeClass('hide');
-//  	}
-// });
+$('.button').click(function() {
+	var objects = ['assets/img/bunny.jpg',
+				   'assets/img/cat.jpg',
+				   'assets/img/dog.jpg',
+				   'assets/img/sloth.jpg',
+				   'assets/img/circle.png',
+				   'assets/img/square.jpg',
+				   'assets/img/triangle.png',
+				    'Meow',
+				    'Meep',
+				    'Woof',
+				    '...'];
+	var colors = [ 'red',
+				  'green',
+				  'blue'
+					];
 
-// $('.nav-item').click(function(){
-// 	var target_horse = $(this).data('horse');
-// 	$(target_horse).removeClass('hide'); 
-// 	// $(target_house... has a dot in it,
-// 	//it's given that's why there's dot in html for apple,nike,disney,vogue
-// });
+    var random_object_number = Math.floor(Math.random() * objects.length);
+	var object_to_use = objects[random_object_number];
+	var result = '';
 
+  	var random_color_numbers = Math.floor(Math.random() * colors.length);
+  	var color_to_use = colors[random_color_numbers];
+  
 
-
-// $('.nav-item').click(function(){
-// 	var target = $(this).data('horse');
-// 	var img_to_show; 
-
-// 	if (target == 'apple'){  //== is checking and = is setting
-// 		img_to_show = '<img class="apple" src="assets/img/Apple.png">';
-// 	} else if (target == 'nike'){
-// 		img_to_show = '<img class="nike" src="assets/img/Nike.png">'
-// 	} else if (target == 'disney'){
-// 		img_to_show = '	<img class="disney" src="assets/img/Disney.png">'
-// 	} else {
-// 		img_to_show = '<img class="vogue" src="assets/img/Vogue.png">'
-// 	}
-
-//  $('.content').append(img_to_show);
-// });
-
-
-
-$('.nav-item').click(function(){
-	var img = $(this).data('img');
-	var class_name = $(this).data('classname');
-	var img_to_show = '	<div class=" ' +class_name+' logo "><img class="apple-logo" src="assets/img/' + img+ ' "/></div>';
-
- $('.content').prepend(img_to_show);
-});
+	if (random_object_number < 7) {
+		result = '<div class="object"><img src="' + object_to_use + '"/></div>';
+	} else{
+		result = '<div class="object ' + color_to_use + '">' + object_to_use + '</div>';
+	}
 
 	
- 	
+ $('.content').prepend(result);
+});
  	
