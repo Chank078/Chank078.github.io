78c49b145877d8a81ffcf830a878eda15bf09d9b
Kimberly Chan <chank078@newschool.edu>
2017-12-05 02:58:18 -0500

    upp

---
+++ track-11/assets/js/app.js
@@ -0,0 +1,4 @@
+function addsparks(){
+	var button=document.createElement("div");
+	button.classList.add("sparks");
+}
---
+++ track-12/assets/js/app.js
@@ -0,0 +1,8 @@
+$('.nav-item').click(function(){
+	var img = $(this).data('img');
+	var class_name = $(this).data('classname');
+	var img_to_show = '	<div class=" ' +class_name+' logo "><img class="Angry-logo" src="assets/img/' + img+ ' "/></div>';
+
+ $('.content').prepend(img_to_show);
+});
+ 	
